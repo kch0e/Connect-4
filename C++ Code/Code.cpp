f34dbd8c93a43157058815fbee7750efe64ebd41
Kevin Choe <kevinkw.choe@gmail.com>
2020-05-03 21:57:23 -0700

    Update Code.cpp

--- C++ Code/Code.cpp
+++ C++ Code/Code.cpp
@@ -1,43 +1,33 @@
 #include <iostream>
 using namespace std;
 
-void createBoard(string board[7][7])
-{
-  for (int i = 0; i < 7; i++)
-  {
-    for (int j = 0; j < 7; j++)
-    {
+void createBoard(string board[7][7]){
+
+  for (int i = 0; i<7; i++){
+    for (int j = 0; j<7; j++){
       board[i][j] = " ";
     }
   }
 }
 
-void displayBoard(string board[7][7])
-{
-  for (int i = 1; i < 8; i++)
-  {
+void displayBoard(string board[7][7]){
+  for (int i = 1; i<8; i++){
     cout << "| " << i << " |";
   }
   cout << "\n";
-  
-  for (int i = 0; i < 7; i++)
-  {
-    for (int j = 0; j < 7; j++)
-    {
+  for (int i = 0; i<7; i++){
+    for (int j = 0; j<7; j++){
       cout << "| " << board[i][j] << " |";
     }
     cout << "\n";
   }
 }
 
-int main()
-{
+int main(){
   int columnY;
-  
-  cout << "Welcome to Connect 4!" << endl;
+  cout << "Time to Play some Connect 4!" << endl;
   string players[2] = {"Player 1", "Player 2"};
   string pMarks[2] = {"X", "O"};
-  
   int moves = 0;
   int top;
   int column;
@@ -47,34 +37,24 @@ int main()
   bool taken[7][7] = {false};
   string board[7][7];
   string winner;
-  
   createBoard(board);
- 
-  while (over == false)
-  {
+  while (over == false){
     placed = false;
     displayBoard(board);
     cout << players[moves%2] << ", pick a column";
     cin >> column;
-    
-    if (taken[0][column-1] == true)
-    {
+    if (taken[0][column-1] == true){
       cout << "Column Full. Play turn again.";
       cin >> column;
     }
-    else if (column<1 || column>7)
-    {
+    else if (column<1 || column>7){
       cout << "This is not a valid move. Play turn again" << endl;
       cin >> column;
     }
-    else 
-    {
-      while (placed == false)
-      {
-        for (int j = 6; j >= 0; j--)
-        {
-          if (taken[j][column-1] == false)
-          {
+    else {
+      while (placed == false){
+        for (int j = 6; j>=0; j--){
+          if (taken[j][column-1] == false){
             board[j][column-1] = pMarks[moves%2];
             placed = true;
             taken[j][column-1] = true;
@@ -85,21 +65,16 @@ int main()
       }
     }
     int noX;
-    for (int i = 0; i<7; i++)
-    {
+    for (int i = 0; i<7; i++){
       noX = 0;
-      for (int j = 0; j<7; j++)
-      {
-        if (board[i][j]== pMarks[moves%2])
-        {
+      for (int j = 0; j<7; j++){
+        if (board[i][j]== pMarks[moves%2]){
           noX++;
         }
-        else 
-        {
+        else {
           noX = 0;
         }
-        if (noX == 4)
-        {
+        if (noX == 4){
           winner = players[moves%2];
           i = 7;
           over = true;
@@ -107,21 +82,16 @@ int main()
         }
       }
     }
-    for (int i = 0; i < 7; i++)
-    {
+    for (int i = 0; i<7; i++){
       noX = 0;
-      for (int j = 0; j < 7; j++)
-      {
-        if (board[j][i]== pMarks[moves%2])
-        {
+      for (int j = 0; j<7; j++){
+        if (board[j][i]== pMarks[moves%2]){
           noX++;
         }
-        else 
-        {
+        else {
           noX = 0;
         }
-        if (noX == 4)
-        {
+        if (noX == 4){
           winner = players[moves%2];
           i = 7;
           over = true;
@@ -130,18 +100,14 @@ int main()
       }
     }
     noX = 0;
-    for (int i = 4; i >- 5; i--)
-    {
-      if (board[columnY-i][column-1-i] == pMarks[moves%2])
-      {
+    for (int i = 4; i>-5; i--){
+      if (board[columnY-i][column-1-i] == pMarks[moves%2]){
         noX++;
       }
-      else 
-      {
+      else {
         noX = 0;
       }
-      if (noX == 4)
-      {
+      if (noX == 4){
         winner = players[moves%2];
         i = -5;
         over = true;
@@ -149,18 +115,14 @@ int main()
       }
     }
     noX = 0;
-    for (int i = 4; i>-5; i--)
-    {
-      if (board[columnY-i][column-1+i] == pMarks[moves%2])
-      {
+    for (int i = 4; i>-5; i--){
+      if (board[columnY-i][column-1+i] == pMarks[moves%2]){
         noX++;
       }
-      else 
-      {
+      else {
         noX = 0;
       }
-      if (noX == 4)
-      {
+      if (noX == 4){
         winner = players[moves%2];
         i = -5;
         over = true;
@@ -168,29 +130,24 @@ int main()
       }
     }
     top = 0;
-    for (int i = 0; i<7; i++)
-    {
-      if (taken[0][i] == true)
-      {
+    for (int i = 0; i<7; i++){
+      if (taken[0][i] == true){
         top++;
       }
-      if (top == 7)
-      {
+      if (top == 7){
         over = true;
         full = true;
         break;
-      }
+      }3
     }
     moves++;
   }
-  if (full == true && winner != players[moves%2])
-  {
+  if (full == true && winner != players[moves%2]){
     displayBoard(board);
-    cout << "Tie Game";
+    cout << "Welp, Guess it's a tie";
   }
-  else
-  {
+  else{
     displayBoard(board);
-    cout << " Congrats! The winner is " << winner << "!";
+    cout << " Congrats!!! The winner is " << winner << "!!!";
   }
 }
